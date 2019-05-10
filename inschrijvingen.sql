6218bf8da2f3bb85804dc52c2400c963788f3cd6
MatthijsH0 <47444041+MatthijsH0@users.noreply.github.com>
2019-05-10 14:52:04 +0200

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -4,3 +4,9 @@ VALUES ('lo9e-amo1', 'brc01', 2019, 'amo'),
        ('lo9e-amo2', 'rs001', 2019, 'amo'),
        ('lo9e-amo3', 'bkr02', 2019, 'amo')
 
+INSERT INTO studenten
+(ov_nummer, achternaam, tussenvoegsel, voorletters, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum)
+VALUES (99009,'huntelaar','','KJ','Klaas-Jan','1993-07-27','2211 SG','Noordwijkerhout', 73.2, 1.86, '2018-01-01'),
+	   (99219,'Vermeer','','K','Kenneth','1992-08-05','2152 EX','Nieuw-Vennep', 82.1, 1.87, '2018-01-01'),
+	   (93213,'Ligt','de','M','Matthijs','1994-03-26','2465 AG','Rijsaterwoude', 67.0, 1.77, '2018-01-01'),
+	   (98765,'Beek','van de','D','Donny','1993-07-13','2321 HP','Leiden', 72.5, 1.85, '2018-01-01')
